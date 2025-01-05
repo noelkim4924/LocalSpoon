29f170cde64d5d105fd3640e4ddd5733821748ea
Noel Kim <wkim51@my.bcit.ca>
2025-01-05 11:35:43 -0800

    folder name changed

--- app/Loading1/page.tsx
+++ app/Loading1/page.tsx
@@ -6,14 +6,15 @@ import StartingLottie from "@/components/Lottie/StartingLottie";
 
 export default function LoadingPage() {
   const router = useRouter();
-
+  
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push("/Home");
-    }, 2000);
+    }, 2100);
 
     return () => clearTimeout(timer); 
   }, [router]);
 
   return <StartingLottie />;
 }
+
