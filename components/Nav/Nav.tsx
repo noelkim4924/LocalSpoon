e7adfb932a993c222e8df530aeb2c40b9fce5bd0
DabeenJung <dabeenj24@gmail.com>
2025-01-05 13:38:20 -0800

    worked on the ranking page css

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -11,6 +11,8 @@ export default function Nav() {
         <Image 
           src="/images/logo.png"
           alt="logo"
+          height={100}
+          width={100}
           className="h-[100px] w-auto"
         />
       </Link>
