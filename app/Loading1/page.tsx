262b0599882261c689d08433a603087f17da332e
Noel Kim <wkim51@my.bcit.ca>
2025-01-05 12:32:28 -0800

    reall

--- app/Loading1/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export const dynamic = "force-dynamic";
-
-import LoadingPage1 from "@/components/LoadingPage1";
-
-export default function Page() {
-  return <LoadingPage1 />;
-}
--- app/Loading2/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export const dynamic = "force-dynamic";
-
-import LoadingPage2 from "@/components/LoadingPage2";
-
-export default function Page() {
-  return <LoadingPage2 />;
-}
