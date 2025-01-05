cc79e2793a8ac6438f2e426676057816e4cf34b8
Noel Kim <wkim51@my.bcit.ca>
2025-01-05 11:49:01 -0800

    fixed deployment second time

--- app/Loading1/page.tsx
+++ app/Loading1/page.tsx
@@ -1,24 +1,27 @@
+// app/Loading2/page.tsx
 "use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
 
-
-const StartingLottie = dynamic(() => import("@/components/Lottie/StartingLottie"), {
+// 동적 임포트 + ssr: false 옵션
+// => 서버 사이드 렌더링 시에는 이 컴포넌트를 로드하지 않음
+const WaitingLottie = dynamic(() => import("@/components/Lottie/WaitingLottie"), {
   ssr: false,
 });
 
-export default function LoadingPage() {
+export default function AnotherLoadingPage() {
   const router = useRouter();
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push("/Home");
-    }, 2100);
+      // 2초 뒤 /Ranking 페이지로 이동
+      router.push("/Ranking");
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, [router]);
 
-  return <StartingLottie />;
+  return <WaitingLottie />;
 }
