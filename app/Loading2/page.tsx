e9174319937ca3fce32ba1721af64225058e784b
Noel Kim <wkim51@my.bcit.ca>
2025-01-05 12:01:23 -0800

    final

--- app/Loading2/page.tsx
+++ app/Loading2/page.tsx
@@ -1,15 +1,22 @@
+// app/Loading2/page.tsx
 "use client";
+export const dynamic = "force-dynamic"; // Next.js 설정(정적 생성 비활성화)
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import WaitingLottie from "@/components/Lottie/WaitingLottie";
+import nextDynamic from "next/dynamic"; // 이름 변경 (기존 'dynamic' -> 'nextDynamic')
+
+// 동적 임포트 + ssr: false 옵션
+const WaitingLottie = nextDynamic(() => import("@/components/Lottie/WaitingLottie"), {
+  ssr: false,
+});
 
 export default function AnotherLoadingPage() {
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push("/ranking"); // 2초 뒤 /Ranking 페이지로 이동
+      router.push("/ranking");
     }, 2000);
 
     return () => clearTimeout(timer);
