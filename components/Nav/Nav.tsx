f96f7b08ccd43633b87acf40c3703ac5c66c6b24
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2025-01-05 11:22:16 -0800

    Image tag update

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Nav() {
   return (
     <nav className="flex items-center  px-[16%] py-2 bg-[#FFF3DE] shadow">
       <Link href="/Home">
-        <img
+        <Image 
           src="/images/logo.png"
           alt="logo"
           className="h-[100px] w-auto"
